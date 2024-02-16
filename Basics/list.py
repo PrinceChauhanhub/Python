ac968e90f8c6424d79012da04a73d2c9bd63ebe6
PrinceChauhanhub <princechauhan220103@gmail.com>
2024-02-16 23:27:52 +0530

    file added

--- Basics/list.py
+++ Basics/list.py
@@ -11,7 +11,7 @@ if "banana" in fruits:
 
 if "kiwi" not in fruits:
     print("kiwi is not present")
-    
+        
 #indexing in list add 
 print(fruits[1])    
 
@@ -20,3 +20,42 @@ print(fruits[-3])
 print(fruits[1:3])    
 
 print(fruits[-3:-1])    
+
+#adding elements to a list 
+
+list =[10,20,30,40]
+
+##append method
+list.append(50)
+print(list)
+
+#insert method
+list.insert(2,25)
+print(list)
+
+list2=[70,80]
+
+#extend method
+list.extend(list2)
+print(list)
+
+
+#removing elements from a list
+#pop method
+list.pop(2)
+print(list)
+
+#remove method
+list.remove(80)
+print(list)
+
+#change item in a list
+#at a index
+
+list[1]=15
+print(list)
+
+#in a range
+list[1:3]=[25,35]
+print(list)
+
---
+++ Basics/swap_list_element.py
@@ -0,0 +1,19 @@
+#given a list and index of elements to swap them
+
+#1
+list=[23,65,19,90]  #idx1=0,inx2=2
+
+temp=list[0]
+list[0]=list[2]
+list[2]=temp
+
+print(list)
+
+#2
+list=[1,2,3,4,5]  #idx1=1,idx2=4
+temp=list[1]
+list[1]=list[4]
+list[4]=temp
+
+print(list)
+
