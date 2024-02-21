45724b239b5fdd7556e616df6d134fb62749a8a9
PrinceChauhanhub <princechauhan220103@gmail.com>
2024-02-21 19:09:33 +0530

    files added

---
+++ Basics/strings.py
@@ -0,0 +1,15 @@
+name1='prince'
+
+name2="india"
+
+#multiline string
+name3='''this is 
+india'''
+
+print(name1,name2,name3)
+
+#type of datatype
+print(type(name1))
+print(type(name2))
+print(type(name3))
+
